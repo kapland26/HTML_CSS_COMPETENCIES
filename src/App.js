2bc4ba96aaab06a4eec7e8150d607517e5f4cd4e
unknown <kapland26backup@gmail.com>
2018-05-23 16:41:54 -0600

    More

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import './box1_3.css';
+import './box4_6.css';
 
 class App extends Component {
   render() {
@@ -8,13 +9,23 @@ class App extends Component {
         <div className="box-one">
           Div 1
         </div>
+
         <div className="box-two">
           <h1> Div 2 </h1>
         </div>
+
         <div className="box-three">
           <div className="elem"/>
           <div className="elem"/>
           <div className="elem"/>
+        </div>
+
+        <div id="box-four">
+          <span>Div 4</span>
+        </div>
+
+        <div className="box-five">
+          <p>Div 5</p>
         </div>
       </div>
     );
