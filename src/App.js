b21d0a4dcfb8952f2256a0da2623545b8600c1ef
unknown <kapland26backup@gmail.com>
2018-05-23 16:18:19 -0600

    Some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="box-one">
+          Div 1
+        </div>
+        <div className="box-two">
+          <h1> Div 2 </h1>
+        </div>
+        <div className="box-three">
+          <div className="elem"/>
+          <div className="elem"/>
+          <div className="elem"/>
+        </div>
       </div>
     );
   }
